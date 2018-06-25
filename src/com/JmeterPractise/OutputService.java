3e85075c274948e1b806c603ca366cf9b3484958
jinwx <jinwx@icity.inesa.com>
2018-06-25 18:00:37 +0800

    Net Project

---
+++ src/com/JmeterPractise/OutputService.java
@@ -0,0 +1,12 @@
+package com.JmeterPractise;
+
+import java.io.File;
+import java.io.PrintWriter;
+
+public class OutputService {  
+    public static void output(String filename,int a, int b) throws Exception {  
+     PrintWriter out = new PrintWriter(new File(filename));  
+     out.write(a+":"+b);  
+     out.close();  
+    }  
+}  
